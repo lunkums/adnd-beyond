86e041199d77ecff0a0da383d1b896956c83845e
Luke Leedy <lukeleedy123@gmail.com>
2023-11-21 17:13:49 -0500

    Add basic webpack project

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/main.ts",
+  devServer: {
+    open: true, // Set to false to prevent the dev server from opening up automatically
+    static: "./dist",
+  },
+  devtool: "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/, // regex to match files with the .ts extension
+        loader: "ts-loader",
+      },
+    ],
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "D&D Above", // Replace this with the name of your app
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+  ],
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
